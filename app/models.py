cfbfb8aecbe554b35688883a4b61fca0354b9a92
Eneko Saez <eneko.saez.merino@opendeusto.es>
2025-04-03 16:12:55 +0200

    models

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, field_validator, Field
+from datetime import datetime
+import math
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Turbina(BaseModel):
+    timestamp: datetime
+    lv_active_power_kw: float = Field(ge=0, le=2000)
+    wind_speed_ms: float = Field(ge=0, le=30)
+    theoretical_power_curve_kwh: float = Field(ge=0, le=2000)    
+    wind_direction_deg: float = Field(ge=0, le=360)
+    
+    @field_validator("wind_speed_ms")
+    def validadar_velocidad_viento(cls, v):
+        if v < 0 or v > 30:
+            logger.error("La velocidad del viento debe estar entre 0 y 30 m/s")
+        return v

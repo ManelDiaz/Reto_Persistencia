57b6ffe76bd1da59abf72fc275ddde756eb84df3
Manel <manel.diaz@opendeusto.es>
2025-04-03 16:00:50 +0200

    Cambio init.sql, se añade drop y quitan comillas

--- db/init.sql
+++ db/init.sql
@@ -1,10 +1,12 @@
-CREATE TABLE IF NOT EXISTS 'turbina'{
-    'timestamp' TIMESTAMP primary key,
-    'lv_active_power_kw' FLOAT,
-    'wind_speed_m_s' FLOAT,
-    'theoretical_power_kw' FLOAT,
-    'wind_direction_deg' FLOAT,
-};
+DROP TABLE IF EXISTS turbina;
 
-CREATE INDEX idx_timestamp ON turbine_data (timestamp); --Para realizar consulta mas rapidas
-CREATE INDEX idx_wind_speed ON turbine_data (wind_speed_ms);
+CREATE TABLE IF NOT EXISTS turbina (
+    timestamp TIMESTAMP PRIMARY KEY,
+    lv_active_power_kw FLOAT,
+    wind_speed_m_s FLOAT,
+    theoretical_power_kw FLOAT,
+    wind_direction_deg FLOAT
+);
+
+CREATE INDEX idx_timestamp ON turbina (timestamp);
+CREATE INDEX idx_wind_speed ON turbina (wind_speed_m_s);

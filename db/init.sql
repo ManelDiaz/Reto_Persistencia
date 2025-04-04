87f93b1a00939f43fcb128a55930efd780362fb9
Manel <manel.diaz@opendeusto.es>
2025-04-04 22:41:34 +0200

    arreglada importación del CSV y inicio de grafana

--- db/init.sql
+++ db/init.sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS turbina;
 CREATE TABLE IF NOT EXISTS turbina (
     timestamp TIMESTAMP PRIMARY KEY,
     lv_active_power_kw FLOAT,
-    wind_speed_m_s FLOAT,
-    theoretical_power_kw FLOAT,
+    wind_speed_ms FLOAT,
+    theoretical_power_curve_kwh FLOAT,
     wind_direction_deg FLOAT
 );
 
 CREATE INDEX idx_timestamp ON turbina (timestamp);
-CREATE INDEX idx_wind_speed ON turbina (wind_speed_m_s);
+CREATE INDEX idx_wind_speed ON turbina (wind_speed_ms);
